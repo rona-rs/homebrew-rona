886addd51129c31a6cec598f5ddb6ee546fc1d0c
TomPlanche <58936594+TomPlanche@users.noreply.github.com>
2025-11-09 16:55:07 +0000

    rona: bump to v2.11.0

--- Formula/rona.rb
+++ Formula/rona.rb
@@ -1,10 +1,10 @@
 class Rona < Formula
   desc "A powerful CLI tool to streamline your Git workflow"
   homepage "https://github.com/rona-rs/rona"
-  url "https://github.com/rona-rs/rona/archive/refs/tags/v2.10.4.tar.gz"
-  sha256 "cd206995298fc00632aa38ef6cb43ed611e498cfc20cfc8a917430de1b268bfd"
+  url "https://github.com/rona-rs/rona/archive/refs/tags/v2.11.0.tar.gz"
+  sha256 "69351223b38232f00ef70dde99f7c5b62203acf5597cde4ff48251fad952a498"
   license "Apache-2.0 OR MIT"
-  version "2.10.4"
+  version "2.11.0"
 
   depends_on "rust" => :build
 
@@ -13,6 +13,6 @@ class Rona < Formula
   end
 
   test do
-    assert_match "rona 2.10.4", shell_output("#{bin}/rona --version")
+    assert_match "rona 2.11.0", shell_output("#{bin}/rona --version")
   end
 end
