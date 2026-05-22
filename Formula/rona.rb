eae7c9e35e67e9bd07bd5b3fd8da1c8dbb7488d2
TomPlanche <58936594+TomPlanche@users.noreply.github.com>
2026-05-22 16:42:21 +0000

    rona: bump to v2.23.0

--- Formula/rona.rb
+++ Formula/rona.rb
@@ -8,9 +8,9 @@ class Rona < Formula
 
   bottle do
     root_url "https://tomplanche.com/api/brew/track/rona"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "896b2f8e1ac01f39bbf2536173e49ce098f9e24717181f02e990c42401247788"
-    sha256 cellar: :any_skip_relocation, sequoia:       "ed2bd8d1d7e95e6e2cfab67b85b71c0aab25c57df5e8152913135b8dcd1f958e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32d8935b7ed89c0ae6821d928c32bdf5e1c5ebda6f077f2c7e5063a21265b7b7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: ""
+    sha256 cellar: :any_skip_relocation, sequoia:       ""
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  ""
   end
 
   depends_on "rust" => :build
